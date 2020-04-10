c95e95f946e16d1dee99adb533d89c554a558a92
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-04-10 11:35:28 +0300

    Added the cart api resources

---
+++ app/Http/Resources/Cart/cartResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Cart;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class cartResource extends Resource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name'=> $this->product->name,
+            'quantity' => $this->quantity,
+            'price'=> $this->product->price,
+        ];
+    }
+}
---
+++ app/model/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    public function product()
+    {
+        return $this->belongsTo('App\model\Product', 'productId', 'id');
+    }
+}
