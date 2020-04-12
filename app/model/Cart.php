3976f0d3f58d85b3e357da6199a3fcecb5eb7376
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-04-12 17:51:43 +0300

    Implemented the Cart And The Reviews Modules Of The API

--- app/model/Cart.php
+++ app/model/Cart.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
+    protected $guarded = ['id','created_at','updated_at'];
+    
     public function product()
     {
         return $this->belongsTo('App\model\Product', 'productId', 'id');
